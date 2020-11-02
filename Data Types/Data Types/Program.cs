0c3f62883468fe9e5ed5122702f3a1791a3fade9
Daniel-Hercules <73712933+Daniel-Hercules@users.noreply.github.com>
2020-11-02 15:39:41 +0000

    Add files via upload

---
+++ Data Types/Data Types/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Different Data Types
+
+            int Integer = 19;
+
+            long LongInteger = 2000;
+
+            double Double = 1.5;
+
+            Char Character = 'D';
+
+            bool Boolean = true;
+
+            String myString = "Hello World";
+
+            Console.WriteLine(Integer + " " +  LongInteger + " " + Double + " " +  Character + " " + Boolean + " " + myString);
+        }
+    }
+}
